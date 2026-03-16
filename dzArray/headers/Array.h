c1898a9b5c267a5ddfe2b17f9532e0d536d72586
Sasha <sashabuzinaevicloud.com>
2026-03-16 16:46:15 +0800

    Added all operators

--- dzArray/headers/Array.h
+++ dzArray/headers/Array.h
@@ -1,5 +1,5 @@
 #pragma once 
-
+const int DEFAULT_CAPACITY=10;
 class Array {
      int* ptr;
     int size, capacity;
@@ -8,6 +8,7 @@ class Array {
      void insert(int elem, int index);
     void remove(int index);
     explicit Array(int startCapacity =DEFAULT_CAPACITY);
+    
 
     ~Array();
     Array(const Array& arr);
