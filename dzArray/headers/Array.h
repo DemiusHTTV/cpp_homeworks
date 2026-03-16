8259f5bc575c6820c9fea38be14c58f77518929d
Sasha <sashabuzinaevicloud.com>
2026-03-16 16:51:16 +0800

    Fix: added namespace std

--- dzArray/headers/Array.h
+++ dzArray/headers/Array.h
@@ -1,17 +1,22 @@
 #pragma once 
+using namespace std;
+
 const int DEFAULT_CAPACITY=10;
 class Array {
      int* ptr;
     int size, capacity;
     public:
-    int& operator[](int index);
-     void insert(int elem, int index);
-    void remove(int index);
-    explicit Array(int startCapacity =DEFAULT_CAPACITY);
-    
-
-    ~Array();
+    explicit Array(int startCapacity = DEFAULT_CAPACITY);
     Array(const Array& arr);
+    ~Array();
+    Array& operator =(const Array& arr);
+    int& operator[](int index);
+    void insert(int elem, int index);
+    void insert(int elem);  
+    void remove(int index);
+    int getSize() const;
+    friend ostream& operator <<(ostream& out, const Array& arr);
+
     
  
-}
+};
