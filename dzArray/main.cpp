d4b369c3aedea1b86d5ac65ea8b55fdd452aa9bf
Sasha <sashabuzinaevicloud.com>
2026-03-16 16:57:53 +0800

    add test for custom Array

--- dzArray/main.cpp
+++ dzArray/main.cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 
 
-Array::Array(int startCapacity){
-    if(startCapacity <=0)
-        capacity = DEFAULT_CAPACITY;
-    else 
-        capacity = startCapacity;
-    ptr = new int[capacity];
-    sizr = 0
- 
-}
-   Array::~Array(){
-        delete[] ptr ;
-    }
-Array::Aray(const Array &arr){
-    
+int main(){
+ setlocale(LC_ALL, "Russian");
+	Array arr(4);
+	for (int i = 0; i < 4; i++)
+		arr.insert(i + 1);
+	cout << arr << endl;
+	for (int i = 0; i < 8; i += 2)
+		arr.insert(10 + i, i);
+	cout << arr << endl;
+	for (int i = 1; i < 8; i += 2)
+		arr[i] = 20 + i;
+	cout << arr << endl;
+	for (int i = 6; i >= 0; i -= 3)
+		arr.remove(i);
+	cout << arr << endl;
 }
