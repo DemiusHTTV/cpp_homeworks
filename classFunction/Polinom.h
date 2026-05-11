7e2cb79dbbabbf15e8803d873d50789e31c13f77
Sanya <sasabuzinaev@gmail.com>
2026-05-11 17:57:03 +0800

    feat: added Polinom

---
+++ classFunction/Polinom.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Function.h"
+#include <vector>
+class Polinom: public Function {
+    std::vector<double> coeffs;
+public:
+    Polinom(double a, double b, double c) : coeffs({a, b, c}) {}
+    double calc(double x) const override{
+        double res =0;
+        for (int i = 0 ; i<coeffs.size(); i++){
+            res += coeffs[i] * pow(x, i);
+        }
+        return res;
+    }
+
+};
