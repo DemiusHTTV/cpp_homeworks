c895b52fe1030e38db71f4777c950ca35a6481f7
DemiusHTTV <102807469+DemiusHTTV@users.noreply.github.com>
2026-05-25 18:33:14 +0800

    Update graph.cpp

--- algoritmsGraphs/graph.cpp
+++ algoritmsGraphs/graph.cpp
@@ -62,56 +62,7 @@ node_iterator Graph::end() {
     return nodes.end();
 }
 
-vector<Graph> Graph::findGraphs() {
-    vector<Graph> result;
-    set<Node*> visited;
 
-    for (node_iterator it = nodes.begin(); it != nodes.end(); it++) {
-        Node* start = *it;
-        if (visited.find(start) != visited.end()) {
-            continue;
-        }
-
-        Graph g;
-        queue<Node*> q;
-        q.push(start);
-
-        while (!q.empty()) {
-            Node* current = q.front();
-            q.pop();
-
-            if (visited.find(current) != visited.end()) {
-                continue;
-            }
-
-            visited.insert(current);
-            Node* newNode = new Node(current->getName());
-            g.addNode(newNode);
-            g.nodeMap[current->getName()] = newNode;
-
-            for (node_iterator nb = current->nb_begin(); nb != current->nb_end(); nb++) {
-                if (visited.find(*nb) == visited.end()) {
-                    q.push(*nb);
-                }
-            }
-        }
-
-        for (node_iterator nit = g.begin(); nit != g.end(); nit++) {
-            Node* oldNode = nodeMap[(*nit)->getName()];
-            for (node_iterator nb = oldNode->nb_begin(); nb != oldNode->nb_end(); nb++) {
-                string nbName = (*nb)->getName();
-                if (g.nodeMap.find(nbName) != g.nodeMap.end()) {
-                    g.addEdge(
-                        g.nodeMap[oldNode->getName()],
-                        g.nodeMap[nbName]
-                    );
-                }
-            }
-        }
-        result.push_back(g);
-    }
-    return result;
-}
 
 void Graph::print() {
     for (node_iterator it = nodes.begin(); it != nodes.end(); it++) {
@@ -123,26 +74,3 @@ void Graph::print() {
     }
 }
 
-void Graph::print2file(string fileName) {
-    ofstream file(fileName);
-    set<string> printed;
-
-    for (node_iterator it = nodes.begin(); it != nodes.end(); it++) {
-        for (node_iterator nb = (*it)->nb_begin(); nb != (*it)->nb_end(); nb++) {
-            string a = (*it)->getName();
-            string b = (*nb)->getName();
-            string edge;
-
-            if (a < b)
-                edge = a + " " + b;
-            else
-                edge = b + " " + a;
-
-            if (printed.find(edge) == printed.end()) {
-                printed.insert(edge);
-                file << edge << endl;
-            }
-        }
-    }
-    file.close();
-}
