a96291e0ac973da701760fac0bc4edc216528873
Sanya <sasabuzinaev@gmail.com>
2026-05-31 12:06:42 +0800

    feat: added dependensies and docs

---
+++ Sudoku/build/CMakeFiles/sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku.dir/Sudoku.cpp.o"
+  "CMakeFiles/sudoku.dir/Sudoku.cpp.o.d"
+  "CMakeFiles/sudoku.dir/main.cpp.o"
+  "CMakeFiles/sudoku.dir/main.cpp.o.d"
+  "sudoku"
+  "sudoku.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
